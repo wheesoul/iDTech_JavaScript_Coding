85197f7df1682eb12de1f88837181621ad9a4b42
wheesoul <olesenkelsey@gmail.com>
2022-06-22 12:57:59 -0700

    JsCode added

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,47 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+
+let firstWords = 'Hi there.';
+let name = 'Trashman';
+let enemy = 'personified pieces of really smelly trash';
+let location = 'New York';
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+
+let numOfEnemies = 2;
+let time = 3;
+let numOfPeopleWalkBy = 100;
+let year = 1998;
+
+
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+
+let defense = [ 'pepper spray', 'brass knuckles', 'lighter'];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+
+let mostPowerful = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+
+console.log( firstWords + " traveler... I assume you've come down here to see me- to see the almighty" + name + "." + "\n");
+console.log("I understand you must be petrified with fear, seeing as you aren't speaking at all, may be due to my ghastly appearance... nevertheless I should tell you the greatest" + "\n");
+console.log("story you shall ever hear in your life:" + "\n");
+console.log("the story of my transformation into the " + name + ".");
+console.log("the year was " + year + " and I had just finished eating my bagel, at the same time I did everyday, " + time + " oclock.");
+console.log("The repition must have brought me to a false state of comfort, a sense of trust in the city of " + location + " that I had never truly recieved back");
+console.log("It's why I wasn't able to see them, the " + enemy + "creeping up to my as I ate my bagel in the subway.");
+console.log("As soon as I had finished the last bite of my delicious, creamy, fufilling bagel, the " + numOfEnemies + enemy + "attacked.");
+console.log("Suddenly, I was engulfed by the worst smelling pieces of trash I had ever encountered in my years as a regular, non-trash human.");
+console.log("Still, I was able to reach into my bag and attempt to grab a weapon, but the only thing I had on me was my " + defense[2] + " .");
+console.log("I believed it was my only way out, but oh how wrong I was... as I ignighted the flame on a piece of the beasts, I suddenly realized they weren't going to move away from me, the trash, plastic, newspapers, were all growing closer to my skin, as the flames burned.")
+console.log("I writhed, trashed, even stopped, dropped, and rolled, but to no avail, the trashed melted me, combined with my skin, and I lay, ashamed, understanding that this would be with me for the rest of my life.");
+console.log("I knew I wasn't dying, the pain had stopped at somepoint, and I opened my eyes to see my suroundings.");
+console.log("At least " + numOfPeopleWalkBy + " people walked past my deformed figure, not a single one ever calling the police or hospital, and my fingers were stuck together and webbed with plastic, I couldn't do anything.");
+console.log("I vowed that day in " + year + " to never let myself be victimized like that again, to become powerful enough that the untrashed humans in " + location + " would face my wrath.");
+console.log("So yes, it is " + mostPowerful + " I will be seeking revenge soon, but there is nothing you can do to stop it.");
